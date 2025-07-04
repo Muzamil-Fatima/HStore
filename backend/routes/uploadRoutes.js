d29b17c63353b67f55fda5f7c64d9a3c4ab767fb
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-07-04 05:41:35 +0500

    Building Product api slice section of fronttend

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,3 +1,4 @@
+import { error } from "console";
 import express from "express";
 import multer from "multer";
 import path from "path";
@@ -15,8 +16,36 @@ const storage = multer.diskStorage({
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\|jpe?g|image\/png|image\/webp/;
+
+  const extname = path.extname(file.originalname).toLowerCase();
+  const mimetype = file.mimetype;
+
+  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single("image");
+
 router.post("/", (req, res) => {
-  res.send("Hello");
+  uploadSingleImage(req, res, (err) => {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    } else if (req.file) {
+      res.status(200).send({
+        message: "Image uploaded successfully",
+        image: `/${req.file.path}`,
+      });
+    } else {
+      res.status(400).send({ message: "No image file provided" });
+    }
+  });
 });
 
 export default router;
---
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useCreateProductMutation,
+  useUploadProductImageMutation,
+} from "../../redux/api/productSlice";
+import { useFetchCategoriesQuery } from "../../redux/api/categorySlice";
+
+const ProductList = () => {
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [brand, setBrand] = useState("");
+  const [stock, setStock] = useState(0);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const navigate = useNavigate();
+
+  const [uploadProductImage] = useUploadProductImageMutation();
+  const [createProduct] = useCreateProductMutation();
+  const { data: categories } = useFetchCategoriesQuery();
+
+  return <div>ProductList</div>;
+};
+
+export default ProductList;
